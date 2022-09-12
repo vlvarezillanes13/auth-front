066ce65d774c057931b07a2a6d3fc727995f724d
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-09-11 21:58:15 -0300

    form reactive

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    name: [ 'Test 3', [Validators.required, ], ],
+    email: [ 'test3@test.com', [Validators.required, Validators.email], ],
+    password: [ '123456', [Validators.required, Validators.minLength(6)], ]
+  })
 
-  ngOnInit(): void {
+  constructor( private fb: FormBuilder) { }
+
+  registrar(): void {
   }
 
 }
